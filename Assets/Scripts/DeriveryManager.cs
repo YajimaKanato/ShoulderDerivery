700be2b715bd1df83794594ca7e5659127abcf86
KNTy1018 <soramametaro1018@gmail.com>
2026-07-15 18:39:11 +0900

    [update]距離と方角がわかるようにした

--- Assets/Scripts/DeriveryManager.cs
+++ Assets/Scripts/DeriveryManager.cs
@@ -1,9 +1,15 @@
+using TMPro;
 using UnityEngine;
 
 public class DeriveryManager : MonoBehaviour
 {
+    [SerializeField] RectTransform _canvas;
+    [SerializeField] TextMeshProUGUI _distanceText;
+    [SerializeField] float _spaceX = 0.8f;
+    [SerializeField] float _spaceY = 0.8f;
     [SerializeField] int _deriveryCount = 5;
     Target[] _targets;
+    Camera _camera;
     int _currentDeriveryIndex = -1;
     int _currentDeriveryCount = 0;
 
@@ -12,9 +18,15 @@ public class DeriveryManager : MonoBehaviour
         Init();
     }
 
+    private void Update()
+    {
+        UpdateDistanceText();
+    }
+
     void Init()
     {
         _targets = FindObjectsByType<Target>(FindObjectsSortMode.None);
+        _camera = Camera.main;
         Shuffle();
         NextDerivery();
     }
@@ -32,7 +44,7 @@ public class DeriveryManager : MonoBehaviour
             target.Init(this);
         }
     }
-            
+
 
     public void NextDerivery()
     {
@@ -41,5 +53,29 @@ public class DeriveryManager : MonoBehaviour
         _currentDeriveryIndex++;
         _currentDeriveryIndex %= _deriveryCount;
         _targets[_currentDeriveryIndex].RequestDerivery();
+    }
+
+    void UpdateDistanceText()
+    {
+        if (_currentDeriveryCount >= _deriveryCount) return;
+        var target = _targets[_currentDeriveryIndex].transform.position;
+        var viewpoint = _camera.WorldToViewportPoint(target);
+        var canvasX = 0f;
+        var canvasY = 1f;
+        if (0 <= viewpoint.x && viewpoint.x <= 1 && viewpoint.z > 0)
+        {
+            // 画面内
+            canvasX = Mathf.Clamp01(viewpoint.x);
+        }
+        else
+        {
+            // 画面外
+            var dir = target - _camera.transform.position;
+            dir.Normalize();
+            canvasX = Vector3.Dot(dir, _camera.transform.right) < 0 ? 0 : 1;
+            canvasY = Vector3.Dot(_camera.transform.forward, dir) * 0.5f + 0.5f;
+        }
+        _distanceText.transform.localPosition = new Vector3((canvasX - 0.5f) * _canvas.rect.width * _spaceX, (canvasY - 0.5f) * _canvas.rect.height * _spaceY, 0);
+        _distanceText.text = Vector3.Distance(_camera.transform.position, target).ToString("0000") + " m";
     }
 }
